0e7f4bdafd857d339da006bf9f371faf7af6131f
MalachyS <98834863+MalachyS@users.noreply.github.com>
2023-05-12 18:19:14 +0100

    Main Functionality Complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Coin } from './coin';
 export class AppComponent implements OnInit {
   coins: Coin[] = [];
   filteredCoins: Coin[] = [];
-  searchQuery = '';
+  isDropdownOpen = false;
   apiKey = 'd855eeb0-d936-44a9-b7ea-8f5cde370ec2';
 
   constructor(private http: HttpClient) {}
@@ -28,12 +28,49 @@ export class AppComponent implements OnInit {
       });
   }
 
-  filterCoins() {
-    this.filteredCoins = this.coins.filter(coin => coin.name.toLowerCase().includes(this.searchQuery.toLowerCase()));
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
   }
 
-  resetFilter() {
-    this.searchQuery = '';
-    this.filteredCoins = [...this.coins];
+  subDropdownOpen: string = '';
+  showSubDropdown(option: string) {
+    this.subDropdownOpen = option;
+  }
+  
+  hideSubDropdown() {
+    this.subDropdownOpen = '';
+  }
+  
+  searchQuery: string = '';
+
+  onSearchQueryChange() {
+    this.filteredCoins = this.coins.filter(
+      coin => coin.name.toLowerCase().includes(this.searchQuery.toLowerCase())
+    );
+  }
+
+  sortByRank() {
+    this.filteredCoins.sort((a, b) => a.rank - b.rank);
+  }
+
+  sortByRankDescending() {
+    this.filteredCoins.sort((a, b) => b.rank - a.rank);
+  }
+
+  sortByPrice() {
+    this.filteredCoins.sort((a, b) => a.priceUsd - b.priceUsd);
+  }
+
+  sortByPriceDescending() {
+    this.filteredCoins.sort((a, b) => b.priceUsd - a.priceUsd);
+  }
+
+  sortByChange() {
+    this.filteredCoins.sort((a, b) => a.changePercent24Hr - b.changePercent24Hr);
+  }
+
+  sortByChangeDescending() {
+    this.filteredCoins.sort((a, b) => b.changePercent24Hr - a.changePercent24Hr);
   }
 }
+
